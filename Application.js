fa4c212e310264107ef04ff834ce15feac4c13fd
XorePwn <Kenan@Sulayman.org>
2011-10-15 03:39:19 +0200

    07cc694b9b3fc636710fa08b6922c42b

---
+++ Application.js
@@ -0,0 +1,11 @@
+// Intialize Listener.
+
+var http = require('http'),
+    faye = require('faye');
+
+bayeux = new faye.NodeAdapter({
+  mount:    '/faye',
+  timeout:  45,
+})
+
+bayeux.listen(8000);
