bbad4a26baaf545fb9fdf39e5943677904bf428d
System Administrator <root@arennes-651-1-107-2.w2-2.abo.wanadoo.fr>
2011-10-17 15:41:09 +0200

    d6f09864fcb39f4a28e5759351973b058342f7352b6cae647a7933911cdf246da47ff6f9510193b1f5b31cf46aead42755773a9b34ba5eaa6d596e56bb35b63b

--- Application.js
+++ Application.js
@@ -62,30 +62,31 @@ function getIP(req) {
 }
 
 function ask(question, format, callback) {
- var stdin = process.stdin, stdout = process.stdout;
- 
- stdin.resume();
- stdout.write(question + ": ");
- 
- stdin.once('data', function(data) {
-   data = data.toString().trim();
- 
-   if (format.test(data)) {
-     callback(data);
-   } else {
-     stdout.write("It should match: "+ format +"\n");
-     ask(question, format, callback);
-   }
- });
+        var stdin = process.stdin,
+                stdout = process.stdout;
+
+        stdin.resume();
+        stdout.write(question + ": ");
+
+        stdin.once('data', function (data) {
+                data = data.toString().trim();
+
+                if (format.test(data)) {
+                        callback(data);
+                } else {
+                        stdout.write("It should match: " + format + "\n");
+                        ask(question, format, callback);
+                }
+        });
 }
 
-ask("Name", /.+/, function(name) {
-  ask("Email", /^.+@.+$/, function(email) {
-    console.log("Your name is: ", name);
-    console.log("Your email is:", email);
- 
-    process.exit();
-  });
+ask("Name", /.+/, function (name) {
+        ask("Email", /^.+@.+$/, function (email) {
+                console.log("Your name is: ", name);
+                console.log("Your email is:", email);
+
+                process.exit();
+        });
 });
 
 var a,b,c,d,e;
